2acdb31a4b517fc9be04f06fe7e09b803c5c1963
arjunaj5 <arjunmuraliknr@gmail.com>
2022-01-28 21:08:51 +0530

    Remove a hardcoded Django secret key

--- plantAI/settings.py
+++ plantAI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@oym)o!im2u!er1q8zqnqhwm3t2)12$d(k&t%(^zz^#uay!e+&'
+# SECRET_KEY = 'django-insecure-@oym)o!im2u!er1q8zqnqhwm3t2)12$d(k&t%(^zz^#uay!e+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
