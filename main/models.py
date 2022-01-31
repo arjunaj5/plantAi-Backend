d5aa85d0c7dde176c392b99a7a43e04f2589686c
arjunaj5 <arjunmuraliknr@gmail.com>
2022-01-31 22:14:49 +0530

    registerd plants model in admin site

--- main/models.py
+++ main/models.py
@@ -7,3 +7,6 @@ class Plants(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image_url = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Plants"
