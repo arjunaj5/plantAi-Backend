70a31ef0695cc8f00c4e20bf4e6b90bbb0641f45
arjunaj5 <arjunmuraliknr@gmail.com>
2022-02-06 22:20:30 +0530

    fix for debug

--- diseases/views.py
+++ diseases/views.py
@@ -18,6 +18,7 @@ class ExampleView(APIView):
 
     def post(self, request):
         data = request.data['photo']
+        print(data)
         format, imgstr = data.split(';base64,', -1)
         ext = format.split('/')[-1]
         data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
