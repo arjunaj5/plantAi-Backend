0a4afb4d91f8fe8e24338f71b9854484cc32fb2d
arjunaj5 <arjunmuraliknr@gmail.com>
2022-02-06 22:41:42 +0530

    fix

--- diseases/views.py
+++ diseases/views.py
@@ -21,7 +21,7 @@ class ExampleView(APIView):
         print(data)
         # format, imgstr = data.split(';base64,', -1)
         # ext = format.split('/')[-1]
-        data = ContentFile(base64.b64decode(data), name='temp.jpeg')
+        data = ContentFile(base64.b64decode(data), name='temp.jpg')
 
         serializer = UploadsSerializer(data={'photo': data})
         if serializer.is_valid():
