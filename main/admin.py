9bbaceccacc21527edb416dd1d4f191eeaabcfae
arjunaj5 <arjunmuraliknr@gmail.com>
2022-02-09 18:40:34 +0530

    fix

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Plants
-from diseases.models import Diseases, Uploads, Cure
+from diseases.models import Diseases, Uploads, Cure, DetectionHistory
 # Register your models here.
 
 admin.site.register(Plants)
 admin.site.register(Diseases)
 admin.site.register(Uploads)
 admin.site.register(Cure)
+admin.site.register(DetectionHistory)
